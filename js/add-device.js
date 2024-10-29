5f7423298c18c0cdf628086a10e75026977c284a
Daniil Dybka <danil.dybko@gmail.com>
2024-10-29 12:33:47 +0300

    Работа с блоком

--- js/add-device.js
+++ js/add-device.js
@@ -1,6 +1,17 @@
 function addDevice() {
+    function HTML({ id, title, parag, img }) {
+        return `
+        <div class="device">
+          <div class="device__left">
+            <h2><span>${id}.</span>${title}</h2>
+            <p>${parag}</p>
+          </div>
+          <img src="/img/${img}" alt="${title}" />
+        </div>`
+    }
+
     const root = document.getElementById('devices-list')
     if (!root) return
 
-    
+
 }
